a73686b3770107e5366a5dc48701400db6337976
Briana Butler <bbutler@webroot.com>
2019-01-09 09:51:57 -0700

    Changed display_raw function to display_raw_data

--- BikeShare_final version.py
+++ BikeShare_final version.py
@@ -207,7 +207,7 @@ def user_stats(df):
     print('-'*40)
 
 
-def display_raw(df):
+def display_raw_data(df):
     """
     Displays only five rows of the data at a time.
     Input:
@@ -267,7 +267,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        display_raw(df)
+        display_raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
